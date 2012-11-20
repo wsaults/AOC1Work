d7a8e8280775d672f6dc9f4d5f8bf651528ee21a
William Saults <wsaults@email.itt-tech.edu>
2012-11-19 21:09:17 -0600

    Minor adjustments.

--- AOC1Work/AOC1Work/ViewController.h
+++ AOC1Work/AOC1Work/ViewController.h
@@ -3,7 +3,8 @@
 //  AOC1Work
 //
 //  Created by Will Saults on 11/19/12.
-//  Copyright (c) 2012 UTVCA. All rights reserved.
+//  AOC1 12/12
+//  Copyright (c) 2012 Fullsail. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
