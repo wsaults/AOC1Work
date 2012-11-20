ae020da7a276153656dff4a88314c543b0f05e7c
William Saults <wsaults@email.itt-tech.edu>
2012-11-19 19:44:23 -0600

    Initial commit.

---
+++ AOC1Work/AOC1Work/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC1Work
+//
+//  Created by Will Saults on 11/19/12.
+//  Copyright (c) 2012 UTVCA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
